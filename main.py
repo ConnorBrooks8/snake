5dcf226eac6b6afe55058219b5464c6b9fcc389e
ConnorBrooks8 <connorbrooks8@gmail.com>
2017-04-20 20:05:41 -0400

    No Collision, No Growth

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ RightKey='Right'
 LeftKey='Left'
 UpKey='Up'
 DownKey='Down'
-DefaultDirection= 'down'
-DefaultHead=[10,10]
+DefaultDirection= 'right'
+DefaultHead=[30,10]
+DefaultEnd=[15,15]
 
 class Snake:
 
@@ -43,35 +44,62 @@ class Snake:
         if event.keysym == DownKey:
             self.direction="down"
 
-    def gameloop(self):
-        while self.quit==False:
-            self.move()
-            time.sleep(0.1)
-
-    def move(self):
-        if self.direction=="right":
-            self.head[0]=self.head[0]+10
-            self.draw()
-        if self.direction=="left":
-            self.head[0]=self.head[0]-10
-            self.draw()
-        if self.direction=="up":
-            self.head[1]=self.head[1]-10
-            self.draw()
-        if self.direction=="down":
-            self.head[1]=self.head[1]+10
-            self.draw()
-    
-    def draw(self):
-        self.canvas.create_rectangle(self.head[0],self.head[1],self.head[0]+10,self.head[1]+10,fill="green")
 
     def startgame(self):
         #clear things
         self.direction= DefaultDirection
         self.head=DefaultHead
+        self.end=DefaultEnd
         self.quit=False
 
+        self.item=self.canvas.create_rectangle(self.head[0],self.head[1],self.head[0]+10,self.head[1]+10,fill="green",tag=DefaultDirection)
+
+        self.canvas.create_rectangle(self.head[0]-10,self.head[1],self.head[0],self.head[1]+10,fill="green",tag="right")
+
+        self.canvas.create_rectangle(self.head[0]-20,self.head[1],self.head[0]-10,self.head[1]+10,fill="green",tag="right")
+
         threading.Thread(target=self.gameloop).start()
+
+
+    def gameloop(self):
+        while self.quit==False:
+            self.move()
+            time.sleep(0.5)
+
+    def move(self):
+        endd = self.canvas.find_closest(self.end[0],self.end[1])[0]
+        
+
+        if self.canvas.gettags(endd)[0]=="right":
+            self.end[0]=self.end[0]+10
+        if self.canvas.gettags(endd)[0]=="left":
+            self.end[0]=self.end[0]-10
+        if self.canvas.gettags(endd)[0]=="up":
+            self.end[1]=self.end[1]-10
+        if self.canvas.gettags(endd)[0]=="down":
+            self.end[1]=self.end[1]+10
+
+        self.canvas.delete(endd)
+        
+        if self.direction=="right":
+            self.head[0]=self.head[0]+10
+            self.canvas.itemconfig(self.item,tag='right')
+            self.draw()
+        if self.direction=="left":
+            self.head[0]=self.head[0]-10
+            self.canvas.itemconfig(self.item,tag='left')
+            self.draw()
+        if self.direction=="up":
+            self.head[1]=self.head[1]-10
+            self.canvas.itemconfig(self.item,tag='up')
+            self.draw()
+        if self.direction=="down":
+            self.head[1]=self.head[1]+10
+            self.canvas.itemconfig(self.item,tag='down')
+            self.draw()
+
+    def draw(self): 
+        self.item = self.canvas.create_rectangle(self.head[0],self.head[1],self.head[0]+10,self.head[1]+10,fill="green")
 
     def quit(self):
         self.quit = True
